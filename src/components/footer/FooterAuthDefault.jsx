8b65ef95e72242f294d524927abe57170e8b4ea0
zahrabenmohamed <benmedzahra20@gmail.com>
2023-04-12 12:01:44 +0000

    clean code v3

--- src/components/footer/FooterAuthDefault.jsx
+++ src/components/footer/FooterAuthDefault.jsx
@@ -4,27 +4,11 @@ export default function Footer() {
   return (
     <div className="z-[5] mx-auto flex w-full max-w-screen-sm flex-col items-center justify-between px-[20px] pb-4 lg:mb-6 lg:max-w-[100%] lg:flex-row xl:mb-2 xl:w-[1310px] xl:pb-6">
       <p className="mb-6 text-center text-sm text-gray-600 md:text-base lg:mb-0">
-        ©{1900 + new Date().getYear()} Horizon UI. All Rights Reserved.
+        ©{1900 + new Date().getYear()}All Rights Reserved.
       </p>
       <ul className="flex flex-wrap items-center sm:flex-nowrap">
-        <li className="mr-12">
-          <a
-            target="blank"
-            href="mailto:hello@simmmple.com"
-            className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
-          >
-            Support
-          </a>
-        </li>
-        <li className="mr-12">
-          <a
-            target="blank"
-            href="https://simmmple.com/licenses"
-            className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
-          >
-            License
-          </a>
-        </li>
+       
+  
         <li className="mr-12">
           <a
             target="blank"
@@ -34,15 +18,7 @@ export default function Footer() {
             Terms of Use
           </a>
         </li>
-        <li>
-          <a
-            target="blank"
-            href="https://blog.horizon-ui.com/"
-            className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
-          >
-            Blog
-          </a>
-        </li>
+       
       </ul>
     </div>
   );
--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -17,38 +17,30 @@ const Dashboard = () => {
     <div>
       {/* Card widget */}
 
-      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
-        <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"Earnings"}
-          subtitle={"$340.5"}
-        />
-        <Widget
-          icon={<IoDocuments className="h-6 w-6" />}
-          title={"Spend this month"}
-          subtitle={"$642.39"}
-        />
-        <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"Sales"}
-          subtitle={"$574.34"}
-        />
-        <Widget
-          icon={<MdDashboard className="h-6 w-6" />}
-          title={"Your Balance"}
-          subtitle={"$1,000"}
-        />
-        <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={"New Tasks"}
-          subtitle={"145"}
-        />
-        <Widget
-          icon={<IoMdHome className="h-6 w-6" />}
-          title={"Total Projects"}
-          subtitle={"$2433"}
-        />
-      </div>
+      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-2 3xl:grid-cols-3">
+    <Widget
+        icon={<MdBarChart className="h-7 w-7" />}
+        title={"Total document"}
+        subtitle={"300"}
+    />
+    <Widget
+        icon={<IoDocuments className="h-6 w-6" />}
+        title={"Total users"}
+        subtitle={"100"}
+    />
+    <Widget
+        icon={<MdBarChart className="h-7 w-7" />}
+        title={"Total templates"}
+        subtitle={"200"}
+    />
+    <Widget
+        icon={<MdDashboard className="h-6 w-6" />}
+        title={"Total Storage used"}
+        subtitle={"200 GB"}
+    />
+</div>
+
+
 
       {/* Charts */}
 
